a5290fdf1ca4fd7edb5156445d22497c99bf3532
alicee88 <alice.easey@gmail.com>
2021-07-25 10:20:02 +0100

    Click to release the ball

--- Bridge-Breaker/Assets/Scripts/Ball.cs
+++ Bridge-Breaker/Assets/Scripts/Ball.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,8 +7,11 @@ public class Ball : MonoBehaviour
 {
     //Configuration parameters
     [SerializeField] Paddle paddle1;
+    [SerializeField] float xPush = 2f;
+    [SerializeField] float yPush = 15f;
 
     Vector2 paddleToBallVector;
+    bool ballReleased = false;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +21,24 @@ public class Ball : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        if (!ballReleased)
+        {
+            LockBallToPaddle();
+            LaunchBallOnClick();
+        }
+    }
+
+    private void LaunchBallOnClick()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+            ballReleased = true;
+        }
+    }
+
+    private void LockBallToPaddle()
     {
         Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
         transform.position = paddlePos + paddleToBallVector;
